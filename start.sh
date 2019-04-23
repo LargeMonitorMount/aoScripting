59a5682d3722a32ecc4f69bc2c32887036c2e053
recska <recskabajnok@gmail.com>
2019-04-23 14:07:01 +0100

    asdasd

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 cd /home/pi/mnt/albioncsoda ;
 python3 dlandconvertocsv.py  > dlresults.txt
-python3 print_me_what_to_Craft.py >results.txt
+python3 print_me_what_to_Craft.py >results.csv
 git add dlresults.txt
-git add results.txt
+git add results.csv
 git commit -m "automated update"
 git push origin master
 
